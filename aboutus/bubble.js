5b44c9a64f4fe9f729f7be7a88f7781192a8277a
Ernie-Wang <erniewangtw@gmail.com>
2018-04-25 00:28:49 +0800

    try to add a class for Bubble, but not working, fix it tomorror

--- aboutus/bubble.js
+++ aboutus/bubble.js
@@ -1,24 +1,50 @@
-
+//----initial function for the bubble, not sure if it's needed or not
 function inti(){
 	var maxBubbles = 40;
 
 	form(action="")
 		for (var b = 0; b < maxBubbles; ++b) {
-			input(type="checkbox",id="bubble" + (b + 1))
-			label(for="bubble" + (b + 1)).bubble
-			span.bubble-inner
-				span
+			input(type="checkbox",id="bubble" + (b + 1));
+			//label(for="bubble" + (b + 1)).bubble;
+			span.bubble-inner;
+			span;
 		}
-	div.timer= "Time"
-    div.time-left
-		span
-	div.score
-	div.intro= "Pop as many bubbles as you can!"
-	div.menu
-    h1= "Game Over"
-    ul
-      li
-        button(type="submit")
-          svg(class="menu-icon",viewBox="0 0 24 24"): path(vector-effect="non-scaling-stroke",fill="#000000",d="M2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2A10,10 0 0,0 2,12M4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12M10,17L15,12L10,7V17Z")/
-          = "Play Again"
 }
+
+
+
+//---try to create a class name Bubble, but failed
+function Bubble() {
+  this.radius = 10;
+  this.position = {x:20, y:20} ;
+  this.style.left = this.position.x+'px';     //bug
+  this.style.top = this.position.y+'px';      //bug
+}
+
+
+
+
+
+
+
+//----sample to create the function for the class Bubble
+
+/*
+Object.assign( Matrix4.prototype, {
+
+    isMatrix4: true,
+
+    set: function ( n11, n12, n13, n14, n21, n22, n23, n24, n31, n32, n33, n34, n41, n42, n43, n44 ) {
+
+      var te = this.elements;
+
+      te[ 0 ] = n11; te[ 4 ] = n12; te[ 8 ] = n13; te[ 12 ] = n14;
+      te[ 1 ] = n21; te[ 5 ] = n22; te[ 9 ] = n23; te[ 13 ] = n24;
+      te[ 2 ] = n31; te[ 6 ] = n32; te[ 10 ] = n33; te[ 14 ] = n34;
+      te[ 3 ] = n41; te[ 7 ] = n42; te[ 11 ] = n43; te[ 15 ] = n44;
+
+      return this;
+
+    },
+*/
+
