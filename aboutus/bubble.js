bf470cd9e29885bf612eb58940eeb2cf025b930d
Ernie-Wang <erniewangtw@gmail.com>
2018-04-25 22:46:59 +0800

    Able to create "bubble" element, wait to finish all the functions for the bubble, and make the bubble more delicate. Now we have two div, one for the bubble style, at the top, the other for the picture, at the bottom. Two Jobs to do, first, finished the functions; second, read bubble.css to know how to make a better looking bubble.

--- aboutus/bubble.js
+++ aboutus/bubble.js
@@ -1,109 +1,128 @@
+var bubble_color = ['f44', 'f84', 'ff8', '8f8', '8ff', '88f', 'f88' ];
+
+
+function Bubble() {
+  this.container = document.createElement( 'div' );     //to create an element for bubble in body   //important!!
+  this.picture_layer = document.createElement( 'div' );     //same as the one above, except this one is for picture
+  document.body.appendChild( this.picture_layer );          // place the element into the document // important!!!
+  document.body.appendChild( this.container );          // place the element into the document // important!!!
+  this.Radius = 10;
+  this.Position = {x:20, y:20} ;
+  this.Destination = {x:20, y:20} ;
+  this.Id = 0;         // the owner of the bubble, from 0~6, initial = 0
+
+  //------general settings for css
+  this.container.style.borderRadius = '50%';
+  this.container.style.opacity = 0.1;
+};
+
+Object.assign( Bubble.prototype, {
+    
+  constructor: Bubble,
+
+  init: function() {
+    this.updateGraph();
+  },
+
+  updateGraph: function(me = this.container) {
+    //var me = this.container;
+    me.style.width = this.Radius+'vw';
+    me.style.height = this.Radius+'vw';
+    me.style.background = '#'+bubble_color[this.Id];
+    
+    //me.style.display= "block";
+
+  },
+
+  setPosition: function(x, y) {       //set the starting point
+    
+  },
+  
+  setRadius: function(R) {            //set the radius of the bubble
+    this.updateGraph();
+  },
+
+  setDestination: function(x, y) {    //the destination of the bubble
+      
+  },
+
+  setColor: function(num) {     //setup the color of the bubble, not background color!
+    this.updateGraph();
+  },
+
+  setID: function(id) {         //set ID
+    this.updateGraph();
+  },
+
+  setBG: function(pic) {        // setup background
+    this.updateGraph();
+  }
+  
+});
+
+/*------------works the same as the functions above, save as backup
+
+var Bubble = function() {
+    this.container = document.createElement( 'div' );
+    this.Radius = '10';
+    var radius = 10;
+    this.Position = {x:20, y:20} ;
+    this.Destination = {x:20, y:20} ;
+    this.Color = 0 ;        // initial color = black
+    this.Id = -1;         // the owner of the bubble, from 0~6, initial = -1
+    //this.style.left = this.position.x+'px';     //bug
+    //this.style.top = this.position.y+'px';      //bug
+    document.body.appendChild( this.container );
+  };
+
+//bject.defineProperty( Bubble, 'id', 1 );
+  Bubble.prototype.init = function() {
+    //var me = this;
+    var rad = this.Radius;
+    //var me = this;
+    this.style.width = rad;
+    //this.style.width = this.Radius;
+    this.style.height = this.Radius;
+    this.style.background = "black";
+  };
+
+
+
+*/
+
+
+//-------------setup function name by yourself
+/*
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (factory((global.BUBBLE = {})));
 }(this, (function (exports) { 'use strict';
 
-
-
-
-
-
-
-
-
-
-
-  var bubble_color = ['f44', 'f84', 'ff8', '8f8', '8ff', '88f', 'f88' ];
-
-
-//----initial function for the bubble, not sure if it's needed or not
-/*
-function inti(){
-	var maxBubbles = 40;
-
-	form(action="")
-		for (var b = 0; b < maxBubbles; ++b) {
-			input(type="checkbox",id="bubble" + (b + 1));
-			//label(for="bubble" + (b + 1)).bubble;
-			span.bubble-inner;
-			span;
-		}
-}
-*/
-
-
-//---try to create a class name Bubble, but failed
   function Bubble() {
+    Object.defineProperty( this, 'id', 1 );
     this.Radius = 10;
     this.Position = {x:20, y:20} ;
     this.Destination = {x:20, y:20} ;
-    this.Color = bubble_color[0] ;        // initial color = black
+    this.Color = 0 ;        // initial color = black
     this.Id = -1;         // the owner of the bubble, from 0~6, initial = -1
     //this.style.left = this.position.x+'px';     //bug
     //this.style.top = this.position.y+'px';      //bug
-  }
+  };
 
-
-  Object.assign( Bubble.prototype, {
-    init: function() {
-
-    },
-  
-    setPosition: function(x, y) {
-      
-    },
-  
-    setRadius: function(R) {
-      
-    },
-
-    setDestination: function(x, y) {
+//Object.defineProperty( Bubble, 'id', 1 );
+  Bubble.prototype.init = function() {
     
-    },
-
-    setColor: function(num) {
-      
-    },
-
-    setID: function(id) {
-      
-    },
-
-    setBG: function(pic) {
-      
-    }
-  
- 
-
-
-  });
+    this.style.width = this.Radius;
+    this.style.height = this.Radius;
+    this.style.background = "black";
+  };
 
 
 })));
 
-//----sample to create the function for the class Bubble
-
-/*
-Object.assign( Matrix4.prototype, {
-
-    isMatrix4: true,
-
-    set: function ( n11, n12, n13, n14, n21, n22, n23, n24, n31, n32, n33, n34, n41, n42, n43, n44 ) {
-
-      var te = this.elements;
-
-      te[ 0 ] = n11; te[ 4 ] = n12; te[ 8 ] = n13; te[ 12 ] = n14;
-      te[ 1 ] = n21; te[ 5 ] = n22; te[ 9 ] = n23; te[ 13 ] = n24;
-      te[ 2 ] = n31; te[ 6 ] = n32; te[ 10 ] = n33; te[ 14 ] = n34;
-      te[ 3 ] = n41; te[ 7 ] = n42; te[ 11 ] = n43; te[ 15 ] = n44;
-
-      return this;
-
-    },
-
-    size: function () {
-      
-    }
 */
+
+
+
 
