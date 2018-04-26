e9d41e8b65490eaae5271526804f13069d829bf8
E24056140 <E24056140@mail.ncku.edu.tw>
2018-04-26 15:30:14 +0800

    20180426 1530

--- aboutus/whale.js
+++ aboutus/whale.js
@@ -1,1 +1,9 @@
-var hr = dt.getSeconds();
+$("#whale").delay(1000).attr("src","whale2.png");
+setInterval(function(){
+  $("#whale").delay(1000).attr("src","whale2.png");
+},2000);
+
+setInterval(function(){
+  $("#whale").attr("src","whale1.png");
+  //$("#whale").delay(1000).attr("src","whale2.png");
+},2000);
