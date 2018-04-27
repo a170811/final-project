085c96015fc19354086c2e988e220758273540b0
E24056140 <E24056140@mail.ncku.edu.tw>
2018-04-27 13:05:21 +0800

    finish whale

--- aboutus/whale.js
+++ aboutus/whale.js
@@ -1,23 +1,13 @@
 var t=0;
 var x=0;
 var y=0;
-var y_square;
-var dir=0;
+var angle=0;
 setInterval(function(){
-  if(t==0){
-    dir=1;
-  }
-  if(x==(2*Math.PI)){
-    dir=0;
-  }
-  if(dir){
-    t+=0.01;
-  }
-  else{
-    t-=0.01;
-  }
-  x=Math.cos(t);
-  y=Math.sin(t);
-  $("#whale").css("right",(x*(($(window).width())/20)));
-  $("#whale").css("top",(y*(($(window).width())/20)));
+  t+=0.01;
+  x=30*Math.cos(t);
+  y=30*Math.sin(t);
+  angle=-(t-(Math.PI/2))
+  $("#whale").css("right",(x*(($(window).width())/100)));
+  $("#whale").css("top",(y*(($(window).width())/100)));
+  $("#whale").css("transform","rotate("+angle+"rad)");
 },30);
