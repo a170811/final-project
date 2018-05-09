5bc1775de7b44ae8a15e26bc9e27c83dbc78882b
Ernie-Wang <erniewangtw@gmail.com>
2018-05-09 22:05:10 +0800

    add server.js, index.html, and insert some basic stuff

---
+++ ajex_test/server.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+const port = 10055
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log( `listening on port: ${port}` )
+})
+
+
