13110b3ad5f0716b9b5e592f2e242547fcc938ce
Ernie-Wang <erniewangtw@gmail.com>
2018-05-10 21:52:49 +0800

    test failed

--- ajex_test/public/index.js
+++ ajex_test/public/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('button[type=submit]').click(function() {
     $.ajax({
       data: {
-        name: $('ajex_form input[name=name]').val()
+        name: $('ajex_form input[name=name]').val(),
         student_id: $('ajex_form input[name=student_id]').val()
       },
       url: 'ajax.php',
