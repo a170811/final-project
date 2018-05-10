9c769cad0531188af12ae7a19a4ce30eb2dc8938
Ernie-Wang <erniewangtw@gmail.com>
2018-05-10 21:58:16 +0800

    add something to ajax.php

--- ajex_test/public/index.js
+++ ajex_test/public/index.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function () {
   $('button[type=submit]').click(function() {
     $.ajax({
@@ -13,3 +14,19 @@ $(document).ready(function () {
     $('#ajax_output').html('loading...');
   });
 });
+*/
+
+$('#ajax-form button[type=submit]').click(e => {
+  e.preventDefault()
+  // no need if button type is not 'submit',
+  //   but make it work without ajax is a good practice
+
+  $.get('ajax.php', {
+    name: $('ajex_form input[name=name]').val(),
+    student_id: $('ajex_form input[name=student_id]').val(),
+  }, (data) => {
+    $('#ajax-output').html(data)
+  })
+  setTimeout(() => $('#ajax-output').html('loading'), 3000)
+  // try to modify the timeout
+})
