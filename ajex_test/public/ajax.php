7ab85e13524928b909923b377b29a1ff6ff52fc8
Ernie-Wang <erniewangtw@gmail.com>
2018-05-10 22:38:32 +0800

    ajax has problem, the word will flash, the code in js says that ajax stop when I trace it, still can't fix

--- ajex_test/public/ajax.php
+++ ajex_test/public/ajax.php
@@ -1,6 +1,6 @@
 <?php
-$name = $_GET["name"];
-$student_id = $_GET["student_id"];
+$name = $_POST["name"];
+$student_id = $_POST["student_id"];
 sleep(2); # pretend to spend some processing time
 echo "Hello, {$name} {$student_id}"; # no <h1> tag in comparison with get/post version
 ?>
--- ajex_test/public/index.js
+++ ajex_test/public/index.js
@@ -1,32 +1,32 @@
-/*
 $(document).ready(function () {
   $('button[type=submit]').click(function() {
     $.ajax({
+      method: "POST",
       data: {
-        name: $('ajex_form input[name=name]').val(),
-        student_id: $('ajex_form input[name=student_id]').val()
+        name: $('#ajex_form input[name=name]').val(),
+        student_id: $('#ajex_form input[name=student_id]').val()
       },
       url: 'ajax.php',
       success: function(data) {
-        $('#ajax_output').text(data);
+        $('#ajax_output').html(data);
       }
     });
     $('#ajax_output').html('loading...');
   });
 });
-*/
-
-$('#ajax-form button[type=submit]').click(e => {
+/*
+$('#ajax_form button[type=submit]').click(e => {
   e.preventDefault()
   // no need if button type is not 'submit',
   //   but make it work without ajax is a good practice
 
   $.get('ajax.php', {
-    name: $('ajex_form input[name=name]').val(),
-    student_id: $('ajex_form input[name=student_id]').val(),
+    name: $('#ajex_form input[name=name]').val(),
+    student_id: $('#ajex_form input[name=student_id]').val()
   }, (data) => {
-    $('#ajax-output').html(data)
+    $('#ajax_output').html(data)
   })
-  setTimeout(() => $('#ajax-output').html('loading'), 3000)
+  setTimeout(() => $('#ajax_output').html('loading'), 3000)
   // try to modify the timeout
 })
+*/
