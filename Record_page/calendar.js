5804d121a8a508391b8f612f7f1a66a75e01192a
E24056140 <E24056140@mail.ncku.edu.tw>
2018-05-15 14:47:29 +0800

    blank calendar

---
+++ Record_page/calendar.js
@@ -0,0 +1,69 @@
+
+var month_olympic = [31,29,31,30,31,30,31,31,30,31,30,31];
+var month_normal = [31,28,31,30,31,30,31,31,30,31,30,31];
+var month_name = ["January","Febrary","March","April","May","June","July","Auguest","September","October","November","December"];
+var holder = document.getElementById("days");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var ctitle = document.getElementById("calendar-title");
+var cyear = document.getElementById("calendar-year");
+var my_date = new Date();
+var my_year = my_date.getFullYear();
+var my_month = my_date.getMonth();
+var my_day = my_date.getDate();
+prev.onclick = function(e){
+	e.preventDefault();
+	my_month--;
+	if(my_month<0){
+		my_year--;
+		my_month = 11;
+	}
+	refreshDate();
+}
+next.onclick = function(e){
+	e.preventDefault();
+	my_month++;
+	if(my_month>11){
+		my_year++;
+		my_month = 0;
+	}
+	refreshDate();
+}
+function refreshDate(){
+	var str = "";
+	var totalDay = daysMonth(my_month, my_year); //获取该月总天数
+	var firstDay = dayStart(my_month, my_year); //获取该月第一天是星期几
+	var myclass;
+	for(var i=1; i<firstDay; i++){ 
+		str += "<li></li>"; //为起始日之前的日期创建空白节点
+	}
+	for(var i=1; i<=totalDay; i++){
+		if((i<my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()) || my_year<my_date.getFullYear() || ( my_year==my_date.getFullYear() && my_month<my_date.getMonth())){ 
+			myclass = " class='darkgrey'"; 
+		}else if (i==my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()){
+			myclass = " class='green greenbox'"; 
+		}else{
+			myclass = " class='lightgrey'"; 
+		}
+		str += "<li"+myclass+">"+i+"</li>"; //创建日期节点
+	}
+	holder.innerHTML = str; //设置日期显示
+	ctitle.innerHTML = month_name[my_month]; //设置英文月份显示
+	cyear.innerHTML = my_year; //设置年份显示
+}
+
+function dayStart(month, year) {
+	var tmpDate = new Date(year, month, 1);
+	return (tmpDate.getDay());
+}
+
+
+function daysMonth(month, year) {
+	var tmp = year % 4;
+	if (tmp == 0) {
+		return (month_olympic[month]);
+	} else {
+		return (month_normal[month]);
+	}
+}
+refreshDate();
