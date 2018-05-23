4df9fc43beaa8f6dc9ec1a29f89f7195137b414c
E24056140 <E24056140@mail.ncku.edu.tw>
2018-05-23 19:56:04 +0800

    fix mobile touch

--- MAIN/Setup_page/music.js
+++ MAIN/Setup_page/music.js
@@ -1,9 +1,9 @@
 var vol;
-$(".slidecontainer").on("swipe",function(){
+document.getElementById("volrange").touchmove=function(){
 	vol=document.getElementById("volrange").value/100;
 	document.getElementById("music").volume=vol; 
 	alert("change");
-}); 
+}
 
 document.getElementById("volrange").onmouseup=function(){
 	vol=document.getElementById("volrange").value/100;
