115b92da8b1bf5aeafcbd8ccdd1c00ff5bc39721
a170811 <a170811@gmail.com>
2018-05-27 18:41:40 +0800

    Daily page(font bug)

--- MAIN/Daily_page/myscript.js
+++ MAIN/Daily_page/myscript.js
@@ -1,1 +1,18 @@
 
+var amount = 0 ;
+
+function click_record() {
+    var board = document.getElementsByClassName('board') ;
+    board[0].style.visibility = 'visible' ;
+    board[0].disabled = true ;
+}
+
+function click_cancel() {
+    var board = document.getElementsByClassName('board') ;
+    board[0].style.visibility = 'hidden' ;
+    board[0].disabled = false ;
+}
+
+function click_comfirm() {
+    
+}
