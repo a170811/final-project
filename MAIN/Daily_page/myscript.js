f0f38b2554a4000868cc66cf9a0f5d66315d927f
a170811 <a170811@gmail.com>
2018-05-28 20:44:57 +0800

    add buttom function

--- MAIN/Daily_page/myscript.js
+++ MAIN/Daily_page/myscript.js
@@ -1,6 +1,6 @@
 
 var amount = 0 ;
-
+var count = 0 ;
 function click_record() {
     var board = document.getElementsByClassName('board') ;
     board[0].style.visibility = 'visible' ;
@@ -11,8 +11,30 @@ function click_cancel() {
     var board = document.getElementsByClassName('board') ;
     board[0].style.visibility = 'hidden' ;
     board[0].disabled = false ;
+    count = 0 ;
+    document.getElementById("count").innerHTML = count ;
 }
 
 function click_comfirm() {
+    amount = count ;
+    alert("you drink "+count.toFixed(1)*1000+" c.c. water") ;
+    count = 0 ;
+    document.getElementById("count").innerHTML = count ;
+    var board = document.getElementsByClassName('board') ;
+    board[0].style.visibility = 'hidden' ;
+    board[0].disabled = false ;
     
+} 
+
+function b1() {
+    count+=0.1 ;
+    document.getElementById("count").innerHTML = count.toFixed(1) ;
+}
+function b2() {
+    count+=0.5 ;
+    document.getElementById("count").innerHTML = count.toFixed(1) ;
+}
+function b3() {
+    count+=1 ;
+    document.getElementById("count").innerHTML = count.toFixed(1) ;
 }
