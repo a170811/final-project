9aa1a05f34cd3aa62e3a0e4584df025a287a287d
E24056140 <E24056140@mail.ncku.edu.tw>
2018-06-10 10:50:33 +0800

    add username at setup

--- MAIN/Setup_page/music.js
+++ MAIN/Setup_page/music.js
@@ -12,3 +12,9 @@ document.getElementById("volrange").ontouchend=function(){
 	vol=document.getElementById("volrange").value/100;
 	document.getElementById("music").volume=vol; 
 }
+var username;
+ FB.api('/me', function(response){
+   username=(response.name);
+   console.log(response.name);//name
+   document.getElementById("hellouser").innerHTML="親愛的"+username+"您好";
+});
