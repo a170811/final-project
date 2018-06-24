607061a97e61bab355f8d7883d929a01f7002715
Ernie-Wang <erniewangtw@gmail.com>
2018-06-24 10:13:11 +0800

    little adjustment

--- MAIN/index.js
+++ MAIN/index.js
@@ -17,9 +17,6 @@ $(document).ready(function () {
  /* 
   // Initialize Firebase
   var config = {
-      apiKey: "AIzaSyDvne8cRLcCzHcgLqE4jdOIeC8fCSI8VJY",
-      authDomain: "uidd2018-groupf.firebaseapp.com",
-      databaseURL: "https://uidd2018-groupf.firebaseio.com",
       projectId: "uidd2018-groupf",
       storageBucket: "uidd2018-groupf.appspot.com",
       messagingSenderId: "363801879198"
